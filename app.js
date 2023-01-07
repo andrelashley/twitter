5721d66e0e7e67c7d33481b3ef94cb73446e31ec
André Lashley <andre.lashley@gmail.com>
2023-01-07 14:28:20 -0800

    Add static files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const middleware = require("./middleware");
+const path = require("path");
 
 const server = app.listen(port, () =>
   console.log("Server listening on port " + port)
@@ -9,6 +10,8 @@ const server = app.listen(port, () =>
 
 app.set("view engine", "pug");
 app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 const loginRoute = require("./routes/loginRoutes");
